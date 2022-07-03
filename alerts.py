a42f301981a798b55e5a92fadab5700b9126fbd3
supriya-mainalli <supriyamainallio@gmail.com>
2022-07-03 22:46:41 +0530

    script for alerts

---
+++ alerts.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+driver.find_element(By.ID, "name").send_keys("supriya")
+
+driver.find_element(By.ID, "alertbtn").click()
+
+alert = driver.switch_to.alert
+
+alert_text = alert.text
+
+if "knowledge" in alert_text:
+    print("The alert text is {}".format(alert_text))
+    alert.accept()  # accept the alert
+
+driver.find_element(By.ID, "confirmbtn").click()
+
+alert2 = driver.switch_to.alert
+
+alert2_text = alert.text
+
+if "confirm" in alert2_text:
+    print("The alert2 text is {}".format(alert2_text))
+    alert2.dismiss()
+    print("Cancelling the alert box")
+
+driver.close()
