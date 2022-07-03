3ca155b7bcee0cb664f6948d784c75b004e72f95
supriya-mainalli <supriyamainallio@gmail.com>
2022-07-03 11:27:57 +0530

    Added locators file

---
+++ locators.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("https://www.rahulshettyacademy.com/angularpractice/")
+
+driver.maximize_window()
+
+driver.find_element_by_name("name").send_keys("Supriya")
+
+driver.find_element_by_name("email").send_keys("abc@gmail.com")
+
+driver.find_element_by_id("exampleInputPassword1").send_keys("1234")
+
+driver.find_element_by_id("exampleCheck1").click()
+
+driver.find_element_by_xpath("//input[@type='submit']").click()
+
+driver.close()
