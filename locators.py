eaea9b2fff745b3f50f48bb204bbf3badaaaa003
Supriya Mainalli <supriyamainallio@gmail.com>
2024-11-01 12:56:20 +0530

    Update locators.py

--- locators.py
+++ locators.py
@@ -1,26 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
-
+# driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+driver = webdriver.Chrome()
 driver.get("https://www.rahulshettyacademy.com/angularpractice/")
 
 driver.maximize_window()
 
-driver.find_element_by_name("name").send_keys("Supriya")
+#TD, xpath, CSS, name, LinkText, class name
+driver.find_element(By.NAME, 'name').send_keys('supriya')
 
-driver.find_element_by_name("email").send_keys("abc@gmail.com")
+driver.find_element(By.NAME,'email').send_keys('abc@gmail.com')
 
-driver.find_element_by_id("exampleInputPassword1").send_keys("1234")
+driver.find_element(By.ID, 'exampleInputPassword1').send_keys('1234')
 
-checkbox = driver.find_element_by_id("exampleCheck1")
+checkbox = driver.find_element(By.ID, 'exampleCheck1')
 checkbox.click()
 
-if not checkbox.is_selected():
-    print("The checkbox is not selected")
-    assert False
+# if not checkbox.is_selected():
+#     print("The checkbox is not selected")
+#     assert False
+#
+# print("The checkbox is selected")
 
-print("The checkbox is selected")
-
-driver.find_element_by_xpath("//input[@type='submit']").click()
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
 
 driver.close()
