adb05eb917183846ca00c9909044c74a7aa48eb5
Supriya Mainalli <supriyamainallio@gmail.com>
2026-05-14 23:02:08 +0530

    update tests

--- 01 browser.py
+++ 01 browser.py
@@ -5,12 +5,9 @@ from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.By import By
 
 driver = webdriver.Chrome()
-driver.get("https://testanild42testt111.freshcmdb.com/support/login")
 driver.maximize_window()
 print(f"The title of the page is : {driver.title}")
 print(f"The currentl url is: {driver.current_url}")
 time.sleep(7)
-driver.find_element(By.ID, 'username').send_keys('sandboxaccountd4223@yopmail.com')
-driver.find_element(By.NAME, 'password').send_keys('test1234')
 driver.find_element(By.XPATH, "//button[@data-testid='login-button']").click()
 time.sleep(5)
